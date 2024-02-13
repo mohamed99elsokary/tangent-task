5b4d921a489b103c82ff384c06dd405e8dac3d59
mohamed99elsokary <mohamed99elsokary@gmail.com>
2024-02-13 14:22:05 +0200

    make card molecule able to handle product object instead of args

--- app/components/molecules/cardMolecule.tsx
+++ app/components/molecules/cardMolecule.tsx
@@ -3,37 +3,39 @@ import colors from "@/app/constants/colors";
 import ButtonAtom from "../atoms/button";
 import ImageAtom from "../atoms/image";
 import TextAtom from "../atoms/text";
-
-interface cardMoleculesInterface {
-  thumbnail: string;
+interface productInterface {
+  id: number;
   title: string;
-  price: number;
   description: string;
+  price: number;
+  thumbnail: string;
+}
+interface cardMoleculesInterface {
+  product: productInterface;
+  onClick: (product: productInterface) => void;
 }
 
 const CardMolecule = ({
-  thumbnail,
-  title,
-  price,
-  description,
+  product,
+  onClick = () => {},
 }: cardMoleculesInterface) => {
   return (
     <>
       <ImageAtom
-        src={thumbnail}
-        alt={`${title} image`}
+        src={product.thumbnail}
+        alt={`${product.title} image`}
         width={300}
         height={300}
       />
-      <TextAtom text={title} />
-      <TextAtom text={description} />
-      <TextAtom text={price} />
+      <TextAtom text={product.title} />
+      <TextAtom text={product.description} />
+      <TextAtom text={product.price} />
       <ButtonAtom
         text="add to cart"
         onClick={() => {
-          console.log("add to cart button called");
+          onClick(product);
         }}
-        color={colors.primary}
+        color={colors.blue}
       />
     </>
   );
