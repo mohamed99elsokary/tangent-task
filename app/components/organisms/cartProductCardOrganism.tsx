b24a58ad7858f3d4c3f8b71bf878b2b4ea3ca6e3
mohamed99elsokary <mohamed99elsokary@gmail.com>
2024-02-13 19:34:01 +0200

    move cart product card to organism

---
+++ app/components/organisms/cartProductCardOrganism.tsx
@@ -0,0 +1,42 @@
+import colors from "@/app/constants/colors";
+import ButtonAtom from "../atoms/button";
+import TextAtom from "../atoms/text";
+import { productInterface } from "@/app/types";
+import { Flex } from "@chakra-ui/react";
+import ProductMolecule from "../molecules/productMolecule";
+
+interface cardMoleculesInterface {
+  product: productInterface;
+  minus: (product: productInterface) => void;
+  plus: (product: productInterface) => void;
+}
+
+const CartProductCardOrganism = ({
+  product,
+  minus = () => {},
+  plus = () => {},
+}: cardMoleculesInterface) => {
+  return (
+    <>
+      <ProductMolecule product={product} />
+      <Flex>
+        <ButtonAtom
+          text="-"
+          onClick={() => {
+            minus(product);
+          }}
+          color={colors.gray}
+        />
+        <TextAtom text={product.quantity} />
+        <ButtonAtom
+          text="+"
+          onClick={() => {
+            plus(product);
+          }}
+          color={colors.gray}
+        />
+      </Flex>
+    </>
+  );
+};
+export default CartProductCardOrganism;
