61c03b019d90a81815da6d0b0bad69e726b7b01a
mohamed99elsokary <mohamed99elsokary@gmail.com>
2024-02-13 20:12:07 +0200

    add max width and max height in image atom

--- app/components/atoms/image.tsx
+++ app/components/atoms/image.tsx
@@ -2,6 +2,17 @@ import Image from "next/image";
 import { imageAtomInterface } from "./types";
 
 const ImageAtom = ({ src, alt, width, height }: imageAtomInterface) => {
-  return <Image src={src} alt={alt} width={width} height={height} />;
+  return (
+    <Image
+      src={src}
+      alt={alt}
+      width={width}
+      height={height}
+      style={{
+        maxWidth: width,
+        maxHeight: height,
+      }}
+    />
+  );
 };
 export default ImageAtom;
