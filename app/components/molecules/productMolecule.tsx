f87ad03cb0fa341e2a251f852ef98dd134969f9c
mohamed99elsokary <mohamed99elsokary@gmail.com>
2024-02-13 20:11:40 +0200

    enhance ProductMolecule looking a bit

--- app/components/molecules/productMolecule.tsx
+++ app/components/molecules/productMolecule.tsx
@@ -1,28 +1,33 @@
+import { Card, CardBody, CardFooter, Stack } from "@chakra-ui/react";
 import ImageAtom from "../atoms/image";
 import TextAtom from "../atoms/text";
-interface productInterface {
-  product: {
-    id: number;
-    title: string;
-    description: string;
-    price: number;
-    thumbnail: string;
-  };
+import { productInterface } from "@/app/types";
+interface ProductMoleculePropsInterface {
+  product: productInterface;
+  children?: React.ReactNode;
 }
 
-const ProductMolecule = ({ product }: productInterface) => {
+const ProductMolecule = ({
+  product,
+  children,
+}: ProductMoleculePropsInterface) => {
   return (
-    <>
-      <ImageAtom
-        src={product.thumbnail}
-        alt={`${product.title} image`}
-        width={300}
-        height={300}
-      />
-      <TextAtom text={product.title} />
-      <TextAtom text={product.description} />
-      <TextAtom text={product.price} />
-    </>
+    <Card maxW="sm">
+      <CardBody>
+        <ImageAtom
+          src={product.thumbnail}
+          alt={`${product.title} image`}
+          width={300}
+          height={300}
+        />
+        <Stack mt="6" spacing="3">
+          <TextAtom text={product.title} />
+          <TextAtom text={product.description} />
+          <TextAtom text={product.price} />
+        </Stack>
+      </CardBody>
+      <CardFooter>{children}</CardFooter>
+    </Card>
   );
 };
 export default ProductMolecule;
